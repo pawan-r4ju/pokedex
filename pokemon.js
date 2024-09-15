d08c1302cea9324c232c8334e8af1aa5971b4bcc
Pawan Raju <115690448+pawan-r4ju@users.noreply.github.com>
2024-09-15 21:31:44 +0530

    Update pokemon.js
    500+ pokemons

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,4 @@
-const MAX_POKEMON = 1025;
+const MAX_POKEMON = 648;
 const listWrapper = document.querySelector(".list-wrapper");
 const searchInput = document.querySelector("#search-input");
 const numberFilter = document.querySelector("#number");
